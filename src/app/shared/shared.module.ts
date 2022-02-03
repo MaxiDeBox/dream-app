6c0729bd320f52d3dee34817e0ba544c257ee38c
Maxim <maxidebox@list.ru>
2022-02-03 11:25:19 +0400

    Реализация табобов - TODO

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { DragDropDirective } from './directives/drag-drop.directive';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ExapmleBoldDirective } from './directives/exapmle-bold.directive';
+import { TabsComponent } from './components/tabs/tabs/tabs.component';
+import { TabComponent } from './components/tabs/tab/tab.component';
 
 const declarations: any[] = [
   DragDropDirective,
@@ -13,18 +15,21 @@ const declarations: any[] = [
   declarations: [
     ...declarations,
     ExapmleBoldDirective,
-
+    TabsComponent,
+    TabComponent,
   ],
   providers: [],
   imports: [
     CommonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   exports: [
     FormsModule,
     ReactiveFormsModule,
-    ...declarations
+    ...declarations,
+    TabsComponent,
+    TabComponent,
   ],
   entryComponents: []
 })
